22e050af7d81da11648890f09fba05c7555923bd
Černý, Adam <cernyad3@fel.cvut.cz>
2023-05-04 10:10:56 +0200

    add data scripts

---
+++ train_FT_model.py
@@ -0,0 +1,52 @@
+import fasttext
+
+import pandas as pd
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import accuracy_score
+
+
+if __name__ == '__main__':
+
+    model = fasttext.train_supervised(
+        input="data/twitter/augmented/full/full/FT_train.txt",
+        autotuneValidationFile="data/twitter/preprocessed/multi/full/FT_valid.txt",
+        #autotuneValidationFile="data/twitter/augmented/full/full//FT_valid.txt",
+        autotuneDuration=600,
+        # autotuneModelSize="20M",
+        #epoch=10,
+        pretrainedVectors="model/unsupervised/wiki.simple.reduced_10.vec",
+        # bucket=1364184,
+        #minn=0,
+        #maxn=0,
+        #lr=0.05,
+        #wordNgrams=0,
+        dim=10,
+    )
+    # save the model
+    model.save_model("model/twitter/new_29_04/model.ftz")
+    print("Testing model precision and recall at one")
+    print(model.test("data/twitter/preprocessed/multi/full/FT_test.txt"))
+
+
+    args_obj = model.f.getArgs()
+    for hparam in dir(args_obj):
+        if not hparam.startswith('__'):
+            print(f"{hparam} -> {getattr(args_obj, hparam)}")
+
+
+    # FT built in classification
+    df = pd.read_pickle(f'data/twitter/preprocessed/multi/full/test.p')
+
+    res = [i for i in list(df.columns) if i.startswith('tweet')][0]
+    df["prediction"] = df[res].apply(lambda x: model.predict(str(x).replace("\n", " "))[0][0][-1:])
+    y_pred = df['prediction'].to_numpy().astype(int)
+    y_test = df['class'].to_numpy().astype(int)
+
+    print(classification_report(y_test, y_pred, digits=3))
+    print(confusion_matrix(y_test, y_pred, normalize='true'))
+
+
+
+
---
+++ tst_FT_model.py
@@ -0,0 +1,36 @@
+import fasttext
+
+import pandas as pd
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import accuracy_score
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+
+    model = fasttext.load_model("spe/spe_classifiers/balanced_full_10_True.ftz")
+
+
+    args_obj = model.f.getArgs()
+    for hparam in dir(args_obj):
+        if not hparam.startswith('__'):
+            print(f"{hparam} -> {getattr(args_obj, hparam)}")
+    # FT built in classification
+    df = pd.read_pickle(f'data/twitter/preprocessed/multi/full/test.p')
+    res = [i for i in list(df.columns) if i.startswith('tweet')][0]
+    df["prediction"] = df[res].apply(lambda x: model.predict(str(x).replace("\n", " "))[0][0][-1:])
+    y_pred = df['prediction'].to_numpy().astype(int)
+    y_test = df['class'].to_numpy().astype(int)
+    print(classification_report(y_test, y_pred, digits=3))
+    conf_mat = confusion_matrix(y_test, y_pred, normalize='true')
+    print(conf_mat)
+
+    # df_cm = pd.DataFrame(conf_mat, index=["hate", "offensive", "neither"],
+    #                      columns=["hate", "offensive", "neither"])
+    # #plt.figure(figsize=(10, 7))
+    # figure = sns.heatmap(df_cm, annot=True, cmap="Blues", cbar=False).get_figure()
+    # figure.savefig('destination_path.eps', format='eps')
+    # figure.savefig('destination_path.png')
